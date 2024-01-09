e65be88b838400edc73884cfac6564137c696f50
Travis <travismollohan44@gmail.com>
2024-01-08 18:37:43 -0700

    User Story 01: Add in hire method functionality, also add in two new methods to facilitate hire functionality.

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -5,35 +5,65 @@ import java.util.Optional;
 public abstract class Organization {
 
     private Position root;
-    
+    private int id = 1;
+
     public Organization() {
         root = createOrganization();
     }
-    
+
     protected abstract Position createOrganization();
-    
+
     /**
      * hire the given person as an employee in the position that has that title
-     * 
+     *
      * @param person
      * @param title
      * @return the newly filled position or empty if no position has that title
      */
     public Optional<Position> hire(Name person, String title) {
-        //your code here
+        return check(root, person, title);
+    }
+
+
+    private Optional<Position> check(Position checkPosition, Name name, String title) {
+        // check title if it matches position title then set employee
+        for (Position p : checkPosition.getDirectReports()) {
+            if (p.getTitle().equals(title)) {
+                return addEmp(p, name);
+            } else if (!p.getDirectReports().isEmpty()) {
+                //if title doesn't match, use the new position, and go through the loop again, until a position is found or the collection is exhausted.
+                Optional<Position> pos = check(p, name, title);
+                if (pos.isPresent()) {
+                    return pos;
+                }
+            }
+        }
         return Optional.empty();
+    }
+
+    private Optional<Position> addEmp(Position position, Name name) {
+        // check if position is filled already
+        if (!position.isFilled()) {
+            //if not filled set the employee in the position
+            position.setEmployee(Optional.of(new Employee(id, name)));
+            id++;
+            return Optional.of(position);
+        } else {
+            throw new IllegalArgumentException("Position is filled");
+        }
     }
 
     @Override
     public String toString() {
         return printOrganization(root, "");
     }
-    
+
     private String printOrganization(Position pos, String prefix) {
         StringBuffer sb = new StringBuffer(prefix + "+-" + pos.toString() + "\n");
-        for(Position p : pos.getDirectReports()) {
+        for (Position p : pos.getDirectReports()) {
             sb.append(printOrganization(p, prefix + "  "));
         }
         return sb.toString();
     }
 }
+
